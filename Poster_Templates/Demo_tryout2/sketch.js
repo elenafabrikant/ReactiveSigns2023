13f52d6b971e2fb6f9222bc187c1ea50a755ae76
elenafabrikant <151722068+elenafabrikant@users.noreply.github.com>
2023-12-11 18:10:41 +0100

    Version Telefonhilfe

--- Poster_Templates/Demo_tryout2/sketch.js
+++ Poster_Templates/Demo_tryout2/sketch.js
@@ -20,9 +20,15 @@ function setup() {
   lines[0] = new symbol(width*0.2, height*0.2)
   lines[0].setEnd(width*0.8,height*0.8)
 
-
   lines[1] = new symbol(width*0.3, height*0.2)
   lines[1].setEnd(width*0.6,height*0.8)
+
+  lines[2] = new symbol(width*0.3, height*0.2)
+  lines[2].setEnd(width*0.3,height*0.9)
+  
+  lines[3] = new symbol(7, 10)
+  lines[3].setEnd(width*0.3,height*0.9)
+
 }
 
 function setuplines(){
@@ -58,49 +64,50 @@ constructor(startx, starty){
   this.animationSpeed = 0.03;
   this.posx = this.startx;
   this.posy = this.startx;
-  //this.posx = this.startx * screen1.w
-  //this.posx = this.starty * screen1.h
+  
+  
 }
 setEnd(endx, endy) {
   this.endx = endx;
   this.endy = endy;
 }
 
-  animateStanding(posX, posY) {
-    push();
-    translate(posX, posY - (vh * 0.25))
-    rotate(PI / 4);
-    line(0, 0, vh * 0.4, vh * 0.4);
-    pop();
-  }
-
 
 display() {
- /* if(poster.position.x >= this.posx-5 && poster.position.x <= this.posx+5) {
-    console.log(this.inStartPos)
-      this.inStartPos =  !this.inStartPos
-  }
 
-  let lerpedPosx = lerp(this.startx, this.endx, this.inStartPos ? 0 : 1);
-  let lerpedPosy = lerp(this.starty, this.endy, this.inStartPos ? 0 : 1);
-*/
-
-  let constrainedX = constrain(poster.posNormal.x, 0.2, 0.8);
+ let constrainedX = constrain(poster.posNormal.x, 0.2, 0.8);
  let lerpedPosx = map(constrainedX, 0.2, 0.8,  this.startx ,  this.endx);
  let lerpedPosy = map(constrainedX, 0.2, 0.8,  this.starty ,  this.endy);
 
     this.posx = lerpedPosx;
     this.posy = lerpedPosy;
 
-    //this.posx = map(this.poster.position.x, 0, 1, this.startx * screen1.w, screen3.x + (this.endx * screen1.w))
-    //this.posy = map(this.poster.position.x, 0, 1, this.starty * screen1.h, screen3.y + (this.endy * screen1.h))
+
+    let dx = this.endx - this.startx;
+    let dy = this.endy - this.starty;
+    let angle = atan2(dy, dx); //angle festlegen
+
+    stroke(255);
+
+    push();
+    translate(this.posx, this.posy);
+    rotate(angle); // Rotate by the calculated angle
+    line(-10, 0, 10, 0); // Drawing a line centered at (0, 0)
+    pop();
+
+    /*push();
+    translate(this.startx, this.starty);
+    let x = this.startx - width / 2;
+    let y = this.starty - height / 2;
+    let a = atan2(y, x);
+    rotate(a);
+
+    line(this.posx-10, this.posy-10, this.posx+10, this.posy+10);
+    pop();
   
   stroke(255) 
-    line(this.posx-10, this.posy, this.posx+10, this.posy);
+    line(this.posx-10, this.posy-10, this.posx+10, this.posy+10);*/
   }
 }
 
-//Questions  luke: how can you make the actual display? Referencing the picture and colours
-//how can you make the lines turn, and the bounce in the end?
-//can we not take the fly programming of github/or what is the difference?
 
